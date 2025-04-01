b419fc1604b339b0c8ef546daa019d7ecd7c3417
JLitxpt <jim.lindkvist@itxpt.org>
2025-04-01 08:14:33 +0200

    First Linden commit

--- validation_json.py
+++ validation_json.py
@@ -1,32 +1,28 @@
 import json
-from jsonschema import ValidationError, Draft202012Validator
-from pathlib import Path
-from referencing import Registry, Resource, Specification
-from referencing.jsonschema import specification_with
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+from jsonschema import Draft202012Validator
+from referencing import Registry, Resource
+from referencing.jsonschema import DRAFT202012
+import os
 
 
-# Path to the JSON file you want to validate
-payload_path = 'examples/S03P01_TiGR/HeaderEvt_EventON_TTS0.json'
-schema_path = 'schemas/S03P01_TiGR/JSONSchema_HeaderEvt_EventON.json'
 
-
-# Load the schemas that are being referenced
-code_referance_path = 'schemas/S03P01_TiGR/JSONSchema_Codes.json'
-header_referance_path = 'schemas/S03P01_TiGR/JSONSchema_HeaderEvt.json'
-
-
-def validate_json(data, schema, referenced_schemas):
+def validate_json(data, schema):
     try:
-        # Create a registry and register the referenced schemas
-        registry = Registry()
-        for uri, schema_content in referenced_schemas.items():
-            registry = registry.with_resource(uri=uri, resource=Resource.from_contents(schema_content))
-
-        # Create the validator with the registry containing multiple referenced schemas
-        validator = Draft202012Validator(schema, registry=registry)
-
+        schema_resource = DRAFT202012.create_resource(schema)
+        codes_resource = DRAFT202012.create_resource(load_json('schemas/S03P01_TiGR/JSONSchema_Codes.json'))
+        header_resource = DRAFT202012.create_resource(load_json('schemas/S03P01_TiGR/JSONSchema_HeaderEvt.json'))
+        registry = Registry().with_resource(uri="urn:schema", resource=schema_resource)
+        registry = registry.with_resource(uri="urn:codes", resource=codes_resource)
+        registry = registry.with_resource(uri="urn:header", resource=header_resource)
+        
+        
+        # Use the registry to validate the instance
+        validator = Draft202012Validator(schema=schema, registry=registry)
         validator.validate(data)
 
+        #print("Valid JSON!")
         return True
     except ValidationError as e:
         print(f"Invalid JSON: {e}")
@@ -34,28 +30,35 @@ def validate_json(data, schema, referenced_schemas):
         print("Schema path: ", e.schema_path)  # Shows the path in the schema that triggered the error
         return False
 
-# Load the schemas that are being referenced
-with open(header_referance_path) as header_schema_file:
-    header_schema = json.load(header_schema_file)
+def load_json(schema_path):
+    with open(schema_path, 'r') as file:
+        schema = json.load(file)
+    return schema
 
-with open(code_referance_path) as code_schema_file:
-    code_schema = json.load(code_schema_file)
+def reject_constants(constant):
+    """Reject invalid JSON constants like NaN, Infinity, -Infinity."""
+    raise ValueError(f"Invalid constant: {constant}")
 
-# Store the referenced schemas in a dictionary with their URIs
-referenced_schemas = {
-    "JSONSchema_HeaderEvt.json": header_schema,
-    "JSONSchema_Codes.json": code_schema,
-}
+def load_json_data(data_path):
+    with open(data_path, 'r') as file:
+        data = json.load(file, parse_constant=reject_constants)
+    return data
 
-# Load the main JSON schema
-with open(schema_path, 'r') as schema_file:
-    schema = json.load(schema_file)
 
-# Load the JSON file you want to validate
 
-with open(payload_path, 'r') as json_file:
-    data = json.load(json_file)
+def print_result(example_path, schema_path):
+    example_data = load_json_data(example_path)
+    schema = load_json(schema_path)
+    if validate_json(example_data, schema):
+        print("Valid JSON")
+    else:
+        print("Invalid JSON!!!")
 
-# Validate the JSON
-if validate_json(data, schema, referenced_schemas):
-    print("PASS: " + payload_path)
+
+print_result('examples/S03P01_TiGR/HeaderEvt_EventLT.json', 'schemas/S03P01_TiGR/JSONSchema_HeaderEvt_EventLT.json')
+print_result('examples/S03P01_TiGR/HeaderEvt_EventOFF_NativeFault.json', 'schemas/S03P01_TiGR/JSONSchema_HeaderEvt_EventOFF.json')
+print_result('examples/S03P01_TiGR/HeaderEvt_EventOFF_TTS0.json', 'schemas/S03P01_TiGR/JSONSchema_HeaderEvt_EventOFF.json')
+print_result('examples/S03P01_TiGR/HeaderEvt_EventON_Extra_NativeFault.json', 'schemas/S03P01_TiGR/JSONSchema_HeaderEvt_EventON_Extra.json')
+print_result('examples/S03P01_TiGR/HeaderEvt_EventON_TTS0.json', 'schemas/S03P01_TiGR/JSONSchema_HeaderEvt_EventON.json')
+print_result('examples/S03P01_TiGR/HeaderEvt_EventTRK.json', 'schemas/S03P01_TiGR/JSONSchema_HeaderEvt_EventTRK.json')
+print_result('examples/S03P01_TiGR/HeaderEvt.json', 'schemas/S03P01_TiGR/JSONSchema_HeaderEvt.json')
